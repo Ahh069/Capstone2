e44a5fb93c7b7d3c1a68f0c97ab8e8c66aea02fa
Ahh069 <mshyt0577@gmail.com>
2024-05-06 20:50:27 -0400

    update

--- Website/src/components/Shop.jsx
+++ Website/src/components/Shop.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import '../styles/Shop.css'; // Add your styles if needed
+// import'../images
 
 function Shop() {
   return (
     <div>
       <div className="Shop">
-        <img className="picture2" src="images/pexels-alteredsnaps-14043693.jpg" alt="Shop Baking Supplies" />
+        <img
+          className="picture2"
+          src="images/pexels-alteredsnaps-14043693.jpg"
+          alt="Shop Baking Supplies"
+        />
         <h2>Place an order</h2>
-        <p>Please let us know what you would like </p>
-        <a href="http://localhost:5050/orders/new" target="_blank" rel="noopener noreferrer">
+        <p>Please let us know what you would like</p>
+        <a
+          href="http://localhost:5050/orders/new"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Order
         </a>
       </div>
+
       <div className="Shop-cake">
         <h2>Gallery</h2>
+        {/* Static gallery of pictures */}
+        <div className="gallery">
+          <img src="images/PXL_20231021_160809326~2.jpg" alt="Cake 1" />
+          <img src="images/cake2.jpg" alt="Cake 2" />
+          <img src="images/cake3.jpg" alt="Cake 3" />
+        </div>
       </div>
     </div>
   );
